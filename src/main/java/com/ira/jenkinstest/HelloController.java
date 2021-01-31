14e3a7eef016839feedb52684532c20f0b035636
ira <s-ira@qq.com>
2021-01-31 22:57:13 +0800

    “初始化工程”

---
+++ src/main/java/com/ira/jenkinstest/HelloController.java
@@ -0,0 +1,17 @@
+package com.ira.jenkinstest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * create by Ira.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello Jenkins";
+    }
+
+}
---
+++ src/main/java/com/ira/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,27 @@
+package com.ira.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class JenkinsTestApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(JenkinsTestApplication.class, args);
+//    }
+//}
+
+
+@SpringBootApplication
+public class JenkinsTestApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsTestApplication.class);
+    }
+}
